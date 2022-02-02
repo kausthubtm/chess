69d461d7ee3610a332b9853f1f1da7dfb41ba945
Kausthub Thekke Madathil <kausthubtm6671@gmail.com>
2022-02-02 17:56:30 +0530

    initial commit

---
+++ chessMain.py
@@ -0,0 +1,97 @@
+import pygame as p
+import chessEngine
+
+WIDTH = HEIGHT = 512  # 400 is another option
+DIMENSIONS = 8  # dinension of a chess board is 8x8
+SQ_SIZE = HEIGHT // DIMENSIONS
+MAX_FPS = 15  # for animations
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(
+            p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE)
+        )
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = chessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False
+
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x,y) location of the mouse
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = chessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    # print(move.getChessNotation())
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        playerClicks = [sqSelected]
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)  # draws squares on the board
+    drawPieces(screen, gs.board)  # draws pieces on top of the squares
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(
+                screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+            )
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(
+                    IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+                )
+
+
+if __name__ == "__main__":
+    main()
